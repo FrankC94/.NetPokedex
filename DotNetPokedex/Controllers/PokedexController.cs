0eb17d7ce40f0a6ac56dca5252d0051c06779a79
FrankC94 <frankc112852@gmail.com>
2018-10-06 04:44:29 -0400

    Updated Pokemon Detail View

--- DotNetPokedex/Controllers/PokedexController.cs
+++ DotNetPokedex/Controllers/PokedexController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DotNetPokedex.Controllers
 {
@@ -7,8 +6,20 @@ namespace DotNetPokedex.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.PokemonName = "Pikachu";
+            ViewBag.PokemonNumber = 25;
+            ViewBag.Description = "<p>Pikachu is a short, chubby rodent Pokémon. It is covered in yellow fur with two horizontal brown stripes on its back. It has a small mouth, long, pointed ears with black tips, brown eyes, and the two red circles on its cheeks contain pouches for electricity storage. </p>";
+            ViewBag.Details = new string[]
+                {
+                    "Category: Mouse",
+                    "Type: Electric",
+                    "Ability: Stattic",
+                    "Weakness: Ground",
+                    "Evolves from: Pichu",
+                    "Evolves to: Raichu"
+                };
             return View();
-           
+
         }
     }
 }
