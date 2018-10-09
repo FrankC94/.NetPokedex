9c78d94bf5b33bbdf3be1f8213ad20e4d9f76eca
FrankC94 <frankc112852@gmail.com>
2018-10-09 06:40:04 -0400

    Updated pokemons controller to use the pokemon repository

---
+++ DotNetPokedex/Controllers/PokemonsController.cs
@@ -0,0 +1,28 @@
+﻿using DotNetPokedex.Data;
+using System.Web.Mvc;
+
+namespace DotNetPokedex.Controllers
+{
+    public class PokemonsController : Controller
+    {
+        private PokemonRepository _pokemonRepository = null;
+
+        public PokemonsController()
+        {
+            _pokemonRepository = new PokemonRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var pokemon = _pokemonRepository.GetPokemon((int)id);
+
+            return View(pokemon);
+
+        }
+    }
+}
---
+++ DotNetPokedex/Data/PokemonRepository.cs
@@ -0,0 +1,73 @@
+﻿using DotNetPokedex.Models;
+
+namespace DotNetPokedex.Data
+{
+    public class PokemonRepository
+    {
+        private static readonly Pokemon[] _pokemons = new Pokemon[]
+        {
+            new Pokemon()
+            {
+                Id = 25,
+                PokemonName = "Pikachu",
+                PokemonNumber = 25,
+                DescriptionHtml = "<p>This Pokémon has electricity-storing pouches on its cheeks. These appear to become electrically charged during the night while Pikachu sleeps. It occasionally discharges electricity when it is dozy after waking up. </p>",
+                Stats = new Stat[]
+                {
+                       new Stat() { Ability = "Ability: Static", Weakness ="Weakness: Ground" },
+
+
+                },
+                Favorite = true
+
+            },
+            new Pokemon()
+            {
+                Id = 26,
+                PokemonName = "Raichu",
+                PokemonNumber = 26,
+                DescriptionHtml = "<p>This Pokémon exudes a weak electrical charge from all over its body that makes it take on a slight glow in darkness. Raichu plants its tail in the ground to discharge electricity. </p>",
+                Stats = new Stat[]
+                {
+                       new Stat() { Ability = "Ability: Static", Weakness ="Weakness: Ground" },
+
+
+                },
+                Favorite = false
+
+            },
+            new Pokemon()
+            {
+                Id = 27,
+                PokemonName = "Sandshrew",
+                PokemonNumber = 27,
+                DescriptionHtml = "<p>Sandshrew has a very dry hide that is extremely tough. The Pokémon can roll into a ball that repels any attack. At night, it burrows into the desert sand to sleep. </p>",
+                Stats = new Stat[]
+                {
+                       new Stat() { Ability = "Ability: Sand Veil", Weakness ="Weakness: Grass, Ice, Water" },
+
+
+                },
+                Favorite = false
+
+            }
+
+         };
+
+        public Pokemon GetPokemon(int id)
+        {
+            Pokemon pokemonToReturn = null;
+
+            foreach (var pokemon in _pokemons)
+            {
+                if (pokemon.Id == id)
+                {
+                    pokemonToReturn = pokemon;
+                    break;
+                }
+
+            }
+            return pokemonToReturn;
+        }
+    }
+}
